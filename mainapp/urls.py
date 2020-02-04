6f224189c6b5552aed0f60398bf0a1f946ace792
ChanOh Jeong <pooo1002@naver.com>
2020-02-04 16:18:55 +0900

    20200204_front

---
+++ mainapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('main/', views.main, name='main'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('swipe/', views.swipe, name='swipe'),
+    path('mypage/', views.mypage, name='mypage'),
+    path('detail/', views.detail, name='detail'),
+    path('like/', views.like, name='like'),
+]
